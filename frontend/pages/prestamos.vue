b20ab37de92753bdf6694fd3aca4b007f8264d99
Jose Cáceres <joseluis8906@opmbx.org>
2017-12-01 22:44:44 -0500

    actualizado

--- frontend/pages/prestamos.vue
+++ frontend/pages/prestamos.vue
@@ -39,7 +39,47 @@ v-layout( align-center justify-center )
       v-card-text
         v-layout( row wrap)
           v-flex( xs12 )
+            v-select( label="Tipo"
+                      :items="Tipos"
+                      v-model="Tipo" )
 
+            v-select( label="Usuario"
+                      :items="Usuarios"
+                      v-show="Tipo==='Por Usuario'"
+                      v-model="Usuario"
+                      item-text="Busqueda"
+                      item-value="Id"
+                      autocomplete )
+
+            v-select( label="Mes"
+                      :items="Meses" )
+
+            v-data-table(v-bind:headers="Headers"
+                      :items="Items"
+                      class="elevation-5 grey lighten-1 grey--text text--darken-4" )
+
+              template(slot="headers" scope="props")
+                th(v-for="header in props.headers" :key="header"
+                  class="text-xs-center") {{ header.text }}
+
+              template(slot="items" scope="props")
+                td(class="text-xs-center" :style="{minWidth: ''+(props.index.toString().length*12)+'px'}") {{ props.index + 1 }}
+                td(class="text-xs-center" :style="{minWidth: ''+(props.item.Material.length*12)+'px'}") {{ props.item.Material }}
+                td(class="text-xs-center" :style="{minWidth: ''+(props.item.Codigo.length*12)+'px'}") {{ props.item.Codigo }}
+                td(class="text-xs-center" :style="{minWidth: ''+(props.item.FechaReserva.length*12)+'px'}") {{ props.item.FechaReserva }}
+                td(class="text-xs-center" :style="{minWidth: ''+(props.item.FechaPrestamo.length*12)+'px'}") {{ props.item.FechaPrestamo }}
+                td(class="text-xs-center" :style="{minWidth: ''+(props.item.FechaDevolucion.length*12)+'px'}") {{ props.item.FechaDevolucion }}
+                td(class="text-xs-center" :style="{minWidth: ''+(props.item.Sancion.length*12)+'px'}") {{ props.item.Sancion }}
+                td(class="text-xs-center")
+                  v-btn(small icon class="teal" dark @click.native="Prestar(props.item)")
+                    v-icon compare_arrows
+                td(class="text-xs-center")
+                  v-btn(small icon class="teal" dark @click.native="Devolver(props.item)")
+                    v-icon compare_arrows
+      v-card-actions
+        v-spacer
+        v-btn( dark @click.native="Reset" ) Cancelar
+        v-btn( dark primary @click.native="Buscar" ) Buscar
 </template>
 
 <style lang="stylus" scoped>
@@ -51,6 +91,8 @@ h5.bold
 </style>
 
 <script>
+import USUARIOS from '~/queries/Usuarios.gql';
+
 export default {
   data () {
     return {
@@ -60,6 +102,39 @@ export default {
         timeout: 6000,
         text: 'Cargando'
       },
+      Tipo: null,
+      Usuario: null,
+      Usuarios: [],
+      Tipos: [
+        'Todos',
+        'Por Usuario'
+      ],
+      Meses: [
+        {text: 'Enero', value: '01'},
+        {text: 'Febrero', value: '02'},
+        {text: 'Marzo', value: '03'},
+        {text: 'Abril', value: '04'},
+        {text: 'Mayo', value: '05'},
+        {text: 'Junio', value: '06'},
+        {text: 'Julio', value: '07'},
+        {text: 'Agosto', value: '08'},
+        {text: 'Septiembre', value: '09'},
+        {text: 'Octubre', value: '10'},
+        {text: 'Noviembre', value: '11'},
+        {text: 'Diciembre', value: '12'},
+      ],
+      Headers: [
+        {text: 'N°', value: ''},
+        {text: 'Material', value: 'Material'},
+        {text: 'Código', value: 'Codigo'},
+        {text: 'F. de Reserva', value: 'FechaReserva'},
+        {text: 'F. de Prestamo', value: 'FechaPrestamo'},
+        {text: 'F. de Devolución', value: 'FechaDevolucion'},
+        {text: 'Sanción', value: 'Sancion'},
+        {text: 'Prestar', value: 'Prestar'},
+        {text: 'Devolución', value: 'Devolucion'},
+      ],
+      Items: [],
       rules: {
         required: (value) => !!value || 'Obligatorio.',
         email: (value) => {
@@ -86,8 +161,56 @@ export default {
   beforeDestroy () {
     this.$mqtt.unsubscribe('b2/apollo/mutation')
   },
+  apollo:{
+    QUsuarios: {
+      query: USUARIOS,
+      loadingKey: 'loading',
+      update (data) {
+        this.Usuarios = [];
+        for(let i=0; i<data.Usuarios.length; i++){
+          let tmp = {
+            Busqueda: data.Usuarios[i].Cedula+' - '+data.Usuarios[i].Nombre+' '+data.Usuarios[i].Apellido,
+            Id: data.Usuarios[i].Id
+          };
+          if(data.Usuarios[i].Cedula !== 'root'){
+            this.Usuarios.push(tmp);
+          }
+        }
+      }
+    }
+  },
   methods: {
-    
+    Reset () {
+
+    },
+    Buscar () {
+      if(this.Tipo === 'Todos'){
+
+        this.$apollo.query({
+          query: PRESTAMOS
+        }).then(res => {
+          console.log(res.data.Prestamos);
+        });
+
+      }else{
+
+        this.$apollo.query({
+          query: PRESTAMOS,
+          variables:{
+            UsuarioId: this.Usuario.Id
+          }
+        }).then(res => {
+          console.log(res.data.Prestamos);
+        });
+
+      }
+    },
+    Prestar () {
+
+    },
+    Devolver () {
+
+    },
   },
   components: {
   }
